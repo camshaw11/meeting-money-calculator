b0cfa85611a962d0ebbcf9826c822fafa7a6004d
itsmattsaudners <itsmattsaunders@gmail.com>
2020-09-16 16:39:57 +1200

    working on the counter

--- client/components/Meeting.jsx
+++ client/components/Meeting.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Meeting (props) {
-  return <div className="container">
-    <h2 className="title is-2">Start Meeting</h2>
-  </div>
+function Meeting(props) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h2 className="title is-2">Start Meeting</h2>
+
+      <h2>the current count is: {count}</h2>
+      <button onClick={counter()}>Click me</button>
+    </div>
+  );
 }
 
-export default connect()(Meeting)
+const counter = (props) => {
+  const { startCount } = this.props;
+  this.setState({
+    count: startCount,
+  });
+  setInterval(this.setState(count + 1), 1000);
+};
+
+// componentDidMount () {
+//   const {startCount} = this.props
+//   this.setState({
+//     count: startCount
+//   })
+//   this.doIntervalChange()
+// }
+
+// doIntervalChange = () => {
+//     this.myInterval = setInterval(() => {
+//     this.setState(prevState => ({
+//       count: prevState.count - 1
+//     }))
+//   }, 1000)
+// }
+
+export default connect()(Meeting);
