1215edf9939ae97fe04714d1cd46c11eea52dd2b
cameron <cameronshaw95@gmail.com>
2020-09-17 08:48:45 +1200

    change to class

--- client/components/History.jsx
+++ client/components/History.jsx
@@ -1,28 +1,30 @@
 import React from "react"
 import { connect } from "react-redux"
 
-function History(props) {
-  return (
-    <div className="container">
-      <h2 className="title is-2">Meeting history</h2>
-      <div className="card">
-        <header className="card-header">
-          <p className="card-header-title">Chicken and Chat</p>
-          <a href="#" className="card-header-icon" aria-label="more options">
-            <span className="icon">
-              <i className="fas fa-angle-down" aria-hidden="true"></i>
-            </span>
-          </a>
-        </header>
-        <div className="card-content">
-          <div className="content">
-            <p>Details{"\n"}</p>
-            <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+class History extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h2 className="title is-2">Meeting history</h2>
+        <div className="card">
+          <header className="card-header">
+            <p className="card-header-title">Chicken and Chat</p>
+            <a href="#" className="card-header-icon" aria-label="more options">
+              <span className="icon">
+                <i className="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </a>
+          </header>
+          <div className="card-content">
+            <div className="content">
+              <p>Details{"\n"}</p>
+              <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default connect()(History)
