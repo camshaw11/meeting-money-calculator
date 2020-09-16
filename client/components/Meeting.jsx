67bbae0dc80b365b14b4c9b1d82c320dd5b12e26
itsmattsaudners <itsmattsaunders@gmail.com>
2020-09-16 17:35:29 +1200

    anthony showed me a cleaner way that makes my brain hurt

--- client/components/Meeting.jsx
+++ client/components/Meeting.jsx
@@ -2,35 +2,17 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import Counter from "./counter";
 
-state = {
-  count: 0,
-};
-
 function Meeting(props) {
   return (
     <div className="container">
       <h2 className="title is-2">Start Meeting</h2>
 
-      <h2>the current count is: {count}</h2>
-      <button onClick={Counter}>Click me</button>
+      {/* <h2>the current count is: {count}</h2>
+      <button onClick={Counter}>Click me</button> */}
+
+      <Counter />
     </div>
   );
 }
-
-// componentDidMount () {
-//   const {startCount} = this.props
-//   this.setState({
-//     count: startCount
-//   })
-//   this.doIntervalChange()
-// }
-
-// doIntervalChange = () => {
-//     this.myInterval = setInterval(() => {
-//     this.setState(prevState => ({
-//       count: prevState.count - 1
-//     }))
-//   }, 1000)
-// }
 
 export default connect()(Meeting);
--- client/components/counter.jsx
+++ client/components/counter.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Counter extends React.Component {
-  counter = (props) => {
-    const { startCount } = this.props;
-    this.setState({
-      count: startCount,
-    });
-    setInterval(this.setState(count + 1), 1000);
+  state = { count: 7521, seconds: 0, minutes: 0, hours: 0 };
+
+  startTimer = () => {
+    setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+
+      const hours = Math.floor(this.state.count / 3600);
+      const minutes = Math.floor((this.state.count - hours * 3600) / 60);
+      const seconds = Math.floor(
+        this.state.count - hours * 3600 - minutes * 60
+      );
+
+      this.setState({ seconds, minutes, hours });
+    }, 1000);
   };
+  componentDidMount() {
+    this.startTimer();
+  }
+
+  render() {
+    return (
+      <p>
+        {this.state.hours}: {this.state.minutes}: {this.state.seconds}
+      </p>
+    );
+  }
 }
 
 export default Counter;
