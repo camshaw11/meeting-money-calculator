31ccc76afca4c1a89de0f4c57cd1b6886e91e8b2
cameron <cameronshaw95@gmail.com>
2020-09-16 16:13:28 +1200

    start table in history page

--- client/components/History.jsx
+++ client/components/History.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from "react"
+import { connect } from "react-redux"
 
-function History (props) {
-  return <div className="container">
-    <h2 className="title is-2">Meeting history</h2>
-  </div>
+function History(props) {
+  return (
+    <div className="container">
+      <h2 className="title is-2">Meeting history</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              <abbr title="Meeting">Meeting</abbr>
+            </th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  )
 }
 
 export default connect()(History)
