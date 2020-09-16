688bd6327edb7c13f3e18882b32fb5cd0bff178b
philc <32604473+Phil160@users.noreply.github.com>
2020-09-17 10:29:21 +1200

    add button to history page to link to meeting details page

--- client/components/History.jsx
+++ client/components/History.jsx
@@ -5,30 +5,42 @@ import PastMeeting from './PastMeeting'
 
 class History extends React.Component {
   state = {
-    page: ""
+    page: "list",
+    currentMeetingId: 0,
+  }
+
+  togglePage = (id) => {
+    this.state.page === "list"
+      ? this.setState({ currentMeetingId: id, page: "details" })
+      : this.setState({ currentMeetingId: id, page: "list" })
   }
 
   render() {
     return (
-      <div className="container">
-        <h2 className="title is-2">Meeting history</h2>
-        <div className="card">
-          <header className="card-header">
-            <p className="card-header-title">Chicken and Chat</p>
-            <a href="#" className="card-header-icon" aria-label="more options">
-              <span className="icon">
-                <i className="fas fa-angle-down" aria-hidden="true"></i>
-              </span>
-            </a>
-          </header>
-          <div className="card-content">
-            <div className="content">
-              <PastMeeting id={1} />Details{"\n"}
-              <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+      this.state.page === "list"
+        ?
+        <div className="container">
+          <h2 className="title is-2">Meeting history</h2>
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">Chicken and Chat</p>
+              <a href="#" className="card-header-icon" aria-label="more options">
+                <span className="icon">
+                  <i className="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+              </a>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                {/* meeting.id taken from a map */}
+                <button onClick={this.togglePage(meeting.id)}>Deetz</button>
+                <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        :
+        <PastMeeting id={this.state.currentMeetingId} />
     )
   }
 }
