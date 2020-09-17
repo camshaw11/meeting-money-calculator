31192108446416ef0afc6dd1bafc618556419e3d
Anthony McGrath <akm@anthonykyle.co.nz>
2020-09-18 08:32:58 +1200

    Update card pointers to actual meeting ID, update date format, add conditional on graph tooltip

--- client/components/PastMeeting.jsx
+++ client/components/PastMeeting.jsx
@@ -1,18 +1,18 @@
-import React from "react"
-import { connect } from "react-redux"
+import React from "react";
+import { connect } from "react-redux";
 
-import { APIgetMeetingDetails } from '../apis/index.js'
-import { togglePage } from '../actions/auth.js'
+import { APIgetMeetingDetails } from "../apis/index.js";
+import { togglePage } from "../actions/auth.js";
 
 class PastMeeting extends React.Component {
   state = {
-    details: '',
-  }
+    details: [],
+  };
 
   componentDidMount() {
-    APIgetMeetingDetails(this.props.id).then(deets => {
-      this.setState({ details: deets })
-    })
+    APIgetMeetingDetails(this.props.id).then((deets) => {
+      this.setState({ details: deets });
+    });
   }
 
   /* <<<<<----- date conversion function for when seconds get passed through, will implement on friday----->>>>>
@@ -25,8 +25,19 @@ class PastMeeting extends React.Component {
         <h2 className="title is-2">Meeting Details</h2>
         <div className="card">
           <header className="card-header">
-            <h4 className="card-header-title">{this.state.details.meeting_name}</h4>
-            <time className="card-header-title level-right" dateTime="">Date: {this.state.details.created_at}</time>
+            <h4 className="card-header-title">
+              {this.state.details.meeting_name}
+            </h4>
+            {this.state.details.created_at && (
+              <time className="card-header-title level-right" dateTime="">
+                Date:{" "}
+                {new Date(this.state.details.created_at).toLocaleString("default", {
+                  dateStyle: "short",
+                  timeStyle: "short",
+                  hourCycle: "h12",
+                })}
+              </time>
+            )}
           </header>
           <div className="card-content">
             <div className="columns is-vcentered">
@@ -34,24 +45,33 @@ class PastMeeting extends React.Component {
                 <h4 className="is-size-5">Cost: ${this.state.details.cost}</h4>
               </div>
               <div className="column">
-                <h4 className="is-size-5">Duration: {this.state.details.duration} hours</h4>
+                <h4 className="is-size-5">
+                  Duration: {this.state.details.duration} hours
+                </h4>
               </div>
               <div className="column">
-                <button className="button is-primary is-light" onClick={() => this.props.dispatch(togglePage("list", 1))}>Hide</button>
+                <button
+                  className="button is-primary is-light"
+                  onClick={() => this.props.dispatch(togglePage("list", 1))}
+                >
+                  Hide
+                </button>
               </div>
             </div>
           </div>
           <footer className="card-footer">
             {this.state.details.attendee_details &&
-              this.state.details.attendee_details.map((attendee, idx) =>
-                <p className="card-footer-item" key={idx}>{attendee.first_name} {attendee.last_name} (${attendee.hourly_wage} p/hr)</p>
-              )
-            }
+              this.state.details.attendee_details.map((attendee, idx) => (
+                <p className="card-footer-item" key={idx}>
+                  {attendee.first_name} {attendee.last_name} ($
+                  {attendee.hourly_wage} p/hr)
+                </p>
+              ))}
           </footer>
         </div>
       </>
-    )
+    );
   }
 }
 
-export default connect()(PastMeeting)
+export default connect()(PastMeeting);
