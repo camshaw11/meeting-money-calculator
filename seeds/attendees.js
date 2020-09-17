401a1eee939921975611b2dcd0e668275dda52fb
Anthony McGrath <akm@anthonykyle.co.nz>
2020-09-18 11:44:54 +1200

    Add Seed Data

--- seeds/attendees.js
+++ seeds/attendees.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('attendees').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('attendees').insert([
         {meeting_id: 1, user_id: 1},
         {meeting_id: 1, user_id: 2},
         {meeting_id: 1, user_id: 3},
